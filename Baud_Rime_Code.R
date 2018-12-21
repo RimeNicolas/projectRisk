c8bff80c002452e466592bf22e71190b80022599
Nicolas <nicolas.rime@epfl.ch>
2018-12-21 09:31:49 +0100

    up to point 5

--- Baud_Rime_Code.R
+++ Baud_Rime_Code.R
@@ -15,9 +15,8 @@ prod = sqrt(cape)*srh
 
 # Define variables
 ppd <- 8 #point per day
-days_y <- 365
 years <- 37
-data_size <- ppd*days*years
+data_size <- ppd*365*years
 
 # Max of 8 points per day to 1 point per day and add date sequence 
 td <- seq(as.Date("1979/01/01"), as.Date("2015/12/31"), "days")
@@ -31,6 +30,7 @@ prod_m <- daily2monthly(prod_day, FUN=max, na.rm=TRUE) #prod max per month
 plot(td, prod_day)
 
 # Convert zoo to numeric
+x_day <- as.numeric(prod_day)
 x_m <- as.numeric(prod_m)
 
 # Plot the monthly maximum
@@ -64,9 +64,10 @@ plot(profile(fit_gev))
 #MCMC
 init <- c(5000,1000,0)
 mat = diag(c(1e11,1e8,1e8))
-pn = prior.norm(mean=c(0,0,0),cov=mat)
+pn = prior.norm(mean=c(0,0,0),cov=mat) # normal distribution
 
-psd = c(800,0.10,0.12) #variance of estimators
+psd = c(800,0.10,0.12) # sd for proposal dist (normal dist)
+# initial value, prior distribution: normal, likelihood: gev, psd: 
 post = posterior(5000,init=init,prior=pn,lh="gev",data=x_m,psd=psd)
 
 #mc = mcmc(post[-c(1:300),])
@@ -82,6 +83,13 @@ acf(MCMC3)
 apply(MCMC3,2,mean)
 apply(MCMC3,2,sd)
 
+# Return level MCMC
+hist(as.numeric(MCMC3[,3]),nclass=20,prob=T,main="Histogram of xi",xlab="xi")
+u.10<-mc.quant(MCMC3,p=0.9,lh="gev")
+u.100<-mc.quant(MCMC3,p=0.99,lh="gev")
+hist(u.10,nclass=20,prob=T,xlab="10-year return level")
+hist(u.100,nclass=20,prob=T,xlab="100-year return level")
+
 ###############################################3
 # r largest statistics per month
 # r = 1, meaning one per month, in order to check with gev and mcmc done previously
@@ -90,7 +98,7 @@ x_m <- matrix(x_m,ncol=1)
 fit0<-rlarg.fit(x_m) # constant
 fit1<-rlarg.fit(x_m,ydat=matrix(months_,ncol=1),mul=c(1)) # linear
 
-# r largest statistics per year
+# r largest statistics per year (point 5, otherwise only 37 points, not enough)
 # r = 1
 time_ <- c(1:years)
 x1 <- apply(matrix(prod,ncol=(ppd*365),byrow=TRUE),1,max)
@@ -131,7 +139,7 @@ plot(time12_,t(x12))
 fit0<-rlarg.fit(x12) # constant
 fit1<-rlarg.fit(x12,ydat=matrix(time_,ncol=r_),mul=c(1)) # linear
 
-# r = 12 same number of points than first part
+# r= 50
 r_ = 50
 time_ <- rep(1:years,each=r_)
 x_ <- matrix(prod,ncol=(ppd*365),byrow=TRUE)
@@ -161,6 +169,91 @@ mat2 <- cbind(x_m,abs(enso))
 plot(mat2)
 cor(mat2)
 
+########################################################################
+# Peaks over threshold
+plot(prod)
+qu.min <- quantile(prod, 0.5) # median value
+qu.max <- quantile(prod,(length(prod)-30)/length(prod))
+print(paste0("median: ",qu.min,", quantile max: ",qu.max))
+mrlplot(prod, tlim=c(qu.min, qu.max))
+par(mfrow=c(1,2))
+tcplot(prod,tlim=c(qu.min, qu.max))
+#or use ismev equivalent
+#gpd.fitrange(prod,umin=qu.min, umax=qu.max)
 
+# Choose threshold by hand giving a good variance bias trade-off
+th <- 1.8e4
+points_year <- 8*365
+fit<-fpot(prod,threshold=th,npp=points_year)
+par(mfrow=c(2,2))
+plot(fit)
+# 1.8e4 seems to be the a good threshold, 1.9e4 is less precise
+th2 <- 1.9e4
+fit2<-fpot(prod,threshold=th2,npp=points_year)
+par(mfrow=c(2,2))
+plot(fit2)
 
+estimatorsPOT = fitted(fit) #estimation of the parameters
+stdPOT = std.errors(fit) #std of the parameters (normal dist.)
+
+# Profile log likelihood 
+par(mfrow=c(1,2))
+plot(profile(fit))
+abline(v=0,col=2,lty=2)
+# Zero shape should be considered
+fit.gum<-fpot(prod, threshold=th, npp=points_year, shape=0)
+# Diagnositc plot, shape zero give good results
+par(mfrow=c(2,2))
+plot(fit.gum)
+estimatorsPOT_0 = fitted(fit.gum)
+stdPOT_0 = std.errors(fit.gum)
+
+######################################################################
+#return level POT shape NON zero
+m10 <- 10*points_year
+m100 <- 100*points_year
+rl10pot <- th + fit$est[1]/fit$est[2]*((m10*fit$pat)^(fit$est[2])-1)
+rl100pot <- th + fit$est[1]/fit$est[2]*((m100*fit$pat)^(fit$est[2])-1)
+
+fit2<-gpd.fit(prod,threshold=th, npy=points_year)
+gpd.diag(fit2)
+
+par(mfrow=c(1,2))
+#Set good lower and upper bound - manually
+gpd.prof(z=fit2, m=10, xlow=2.8e4, xup=3.5e4, npy = points_year, conf = 0.95)
+title("Profile Log-likelihood \n of 10-year Return Level")
+abline(v=rl10pot)
+gpd.prof(z=fit2, m=100, xlow=3.25e4, xup=4.8e4, npy = points_year, conf = 0.95)
+title("Profile Log Likelihood \n of 100-year Return Level")
+abline(v=rl100pot) #must coincide with MLE
+
+#return level POT shape = 0
+rl10_0 <- th + fit.gum$est[1]*log((m10*fit.gum$pat))
+rl100_0 <- th + fit.gum$est[1]*log((m100*fit.gum$pat))
+
+###################################################################
+# Poisson process
+# NOT working with initial threshold th with initial threshold
+# information matrix singular
+
+# try higher threshold 0.98 quantile
+# keep this threshold, quantile 0.99 not working
+th3 <-quantile(prod,0.98)
+fit3<-pp.fit(prod,threshold=th3, npy=points_year)
+par(mfrow=c(2,2))
+pp.diag(fit3)
+
+# compute nb obs above threshold
+n <- 0
+for (i in c(1:data_size)){
+  if (prod[i] > th3){n <- n+1}
+}
+zeta <- n/data_size
+
+#return level PP shape NON zero
+scale_ <- fit3$mle[2] + (th3 - fit3$mle[1])*fit3$mle[3]
+rl10pp <- th + scale_/fit3$mle[3]*((m10*zeta)^(fit3$mle[3])-1)
+rl100pp <- th + scale_/fit3$mle[3]*((m100*zeta)^(fit3$mle[3])-1)
+
+################################################################
 
